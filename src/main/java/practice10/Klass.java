5cda1242ca89c5afe9d1ea49f25db26152f6a005
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 20:42:07 +0800

    [Java OOP] [John Emmanuel] feature: implement changes applied in practice09 branch

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,41 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> studentList;
+
+    public Klass(int number) {
+        this.number = number;
+        studentList = new ArrayList<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        if (!studentList.contains(student)) {
+            System.out.print("It is not one of us.\n");
+
+            return;
+        }
+
+        leader = student;
+    }
+
+    public void appendMember(Student student) {
+        studentList.add(student);
+    }
 }
