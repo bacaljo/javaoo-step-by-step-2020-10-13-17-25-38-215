a51079063ba06da06b08e0dc3e6805630c73935d
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 18:53:10 +0800

    [Java OOP] [John Emmanuel] feature: enhance introduce method in Teacher to display a different message if teacher has no class

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -3,6 +3,10 @@ package practice08;
 public class Teacher extends Person {
     private Klass klass;
 
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
     public Teacher(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
@@ -14,7 +18,10 @@ public class Teacher extends Person {
 
     @Override
     public String introduce() {
+        boolean hasAClass = klass != null;
+
         return String.format("%s I am a Teacher. I teach %s.",
-                super.introduce(), klass.getDisplayName());
+                super.introduce(),
+                (hasAClass) ? klass.getDisplayName() : "No Class");
     }
 }
