26a2fe58016b21983a1a02b92a5b027a3ce39bed
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 18:31:50 +0800

    [Java OOP] [John Emmanuel] feature: make Student inherit Person and create a klass: Klass attribute with a getter; create a leader: Student attribute with getters and setters in Klass

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -2,6 +2,7 @@ package practice08;
 
 public class Klass {
     private int number;
+    private Student leader;
 
     public Klass(int number) {
         this.number = number;
@@ -13,5 +14,13 @@ public class Klass {
 
     public String getDisplayName() {
         return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
     }
 }
