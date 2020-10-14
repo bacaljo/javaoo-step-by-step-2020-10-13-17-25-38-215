6d79ec98a43d96ab5065f0923bef5f9c9c34e9c1
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 16:32:20 +0800

    [Java OOP] [John Emmanuel] feature: overload constructor of Teacher that will only accept name and age and enhance introduce method to display a different message if there is no given klass

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -3,6 +3,11 @@ package practice06;
 public class Teacher extends Person {
     private int klass;
 
+    public Teacher(String name, int age) {
+        super(name, age);
+        klass = -1;
+    }
+
     public Teacher(String name, int age, int klass) {
         super(name, age);
         this.klass = klass;
@@ -14,7 +19,9 @@ public class Teacher extends Person {
 
     @Override
     public String introduce() {
-        return String.format("%s I am a Teacher. I teach Class %d.",
-                super.introduce(), klass);
+        return String.format("%s I am a Teacher. I teach %s.",
+                super.introduce(), (klass >= 0)
+                        ? "Class " + klass
+                        : "No Class");
     }
 }
