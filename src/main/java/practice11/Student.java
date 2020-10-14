7d5d9bbe19e883cf34a72c5b5367272b094ab2ff
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 21:43:16 +0800

    [Java OOP] [John Emmanuel] feature: implement changes applied in practice10 branch

--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,24 @@
 package practice11;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        boolean isClassLeader = klass.getLeader() != null && klass.getLeader().equals(this);
+
+        return String.format("%s I am a Student. I am %s %s.",
+                super.introduce(),
+                isClassLeader ? "Leader of" : "at",
+                klass.getDisplayName());
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,45 @@
 package practice11;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private List<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        classes = new ArrayList<>();
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        boolean hasAnyClass = !classes.isEmpty();
+        String classesDisplayName = (hasAnyClass)
+                ? String.format("Class %s", classes.stream().map(klass -> Integer.toString(klass.getNumber())).collect(Collectors.joining(", ")))
+                : "No Class";
+
+        return String.format("%s I am a Teacher. I teach %s.",
+                super.introduce(), classesDisplayName);
+    }
+
+    public boolean isTeaching(Student student) {
+        return classes.contains(student.getKlass());
+    }
+
+    public String introduceWith(Student student) {
+        return String.format("%s I am a Teacher. I %s %s.",
+                super.introduce(),
+                (isTeaching(student)) ? "teach" : "don't teach",
+                student.getName());
+    }
 }
