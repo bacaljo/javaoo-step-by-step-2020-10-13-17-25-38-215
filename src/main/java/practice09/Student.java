0922c033b8e4dbbbbd33979d77c07f8828616e14
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 19:20:18 +0800

    [Java OOP] [John Emmanuel] feature: implement changes applied in practice08 branch

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,20 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Student. I am at %s.",
+                super.introduce(), klass.getDisplayName());
+    }
 }
