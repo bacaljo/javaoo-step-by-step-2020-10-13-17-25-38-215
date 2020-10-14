6d9935fc319d9e77ff96238dd0e862a6ebe17153
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 15:47:10 +0800

    [Java OOP] [John Emmanuel] feature: implement changes applied in practice02 branch

--- src/test/java/practice03/Practice03Test.java
+++ src/test/java/practice03/Practice03Test.java
@@ -18,20 +18,20 @@ public class Practice03Test {
         String introduce = tom.introduce();
         assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
     }
-//
-//    @Test
-//    public void should_student_have_name_age_and_class_number() throws Exception {
-//        Student tom = new Student("Tom", 21, 2);
-//        assertThat(tom.getName()).isEqualTo("Tom");
-//        assertThat(tom.getAge()).isEqualTo(21);
-//        assertThat(tom.getKlass()).isEqualTo(2);
-//    }
-//
-//    @Test
-//    public void should_student_introduce_with_class() throws Exception {
-//        Student tom = new Student("Tom", 21, 2);
-//        assertThat(tom.introduce()).isEqualTo("I am a Student. I am at Class 2.");
-//    }
+
+    @Test
+    public void should_student_have_name_age_and_class_number() throws Exception {
+        Student tom = new Student("Tom", 21, 2);
+        assertThat(tom.getName()).isEqualTo("Tom");
+        assertThat(tom.getAge()).isEqualTo(21);
+        assertThat(tom.getKlass()).isEqualTo(2);
+    }
+
+    @Test
+    public void should_student_introduce_with_class() throws Exception {
+        Student tom = new Student("Tom", 21, 2);
+        assertThat(tom.introduce()).isEqualTo("I am a Student. I am at Class 2.");
+    }
 //
 //    @Test
 //    public void should_worker_have_name_and_age() throws Exception {
