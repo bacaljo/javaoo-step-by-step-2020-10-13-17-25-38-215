0a57a48fd230fd94ef8d45c46083c59b1fd05020
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 15:32:28 +0800

    [Java OOP] [John Emmanuel] feature: create introduce method in Person class and display the correct introduction

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -3,6 +3,7 @@ package practice01;
 public class Person {
     private String name;
     private int age;
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
@@ -14,5 +15,10 @@ public class Person {
 
     public int getAge() {
         return age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.",
+                name, age);
     }
 }
